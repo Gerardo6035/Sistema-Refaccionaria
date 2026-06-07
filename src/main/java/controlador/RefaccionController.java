b2ac7db98438ff28a70b92f7e80a92a3b8f51c10
Gerardo6035 <luisgerardo6035@gmail.com>
2026-06-06 19:55:25 -0600

    Add delete and update functionality to RefaccionController

--- src/main/java/controlador/RefaccionController.java
+++ src/main/java/controlador/RefaccionController.java
@@ -62,6 +62,50 @@ public class RefaccionController extends HttpServlet
             //Regresa a la pantalla del inventario para que se la nueva pieza en la tabla
             response.sendRedirect("RefaccionController?accion=listar");
         }
+        else if (accion.equals("borrar")) 
+        {
+            //Atrapa el id de la pieza que viene desde el enlace de la tabla
+            int id = Integer.parseInt(request.getParameter("id"));
+            
+            //Manda el id al DAO para que ejecute el DELETE en PostgreSQL
+            rDao.eliminar(id);
+            
+            //Vuelve al listado para refrescar la tabla y ver el cambio
+            response.sendRedirect("RefaccionController?accion=listar");
+        }
+        else if (accion.equals("editar"))
+        {
+            //Atrapa el id de la pieza que se va a modificar
+            int id = Integer.parseInt(request.getParameter("id"));
+            
+            //Busca los datos actuales de la pieza en la bd
+            Refaccion ref = rDao.listarId(id);
+            
+            //Comparte los datos encontrados para que el formulario los dibuje
+            request.setAttribute("refaccionEditar", ref);
+            request.getRequestDispatcher("editarRefaccion.jsp").forward(request, response);
+        }
+        else if (accion.equals("actualizar"))
+        {
+            //Atrapa todos los datos modificados del formulario incluyendo el id oculto
+            int id = Integer.parseInt(request.getParameter("txtId"));
+            String codigo = request.getParameter("txtCodigo");
+            String nombre = request.getParameter("txtNombre");
+            String descripcion = request.getParameter("txtDescripcion");
+            
+            double precio = Double.parseDouble(request.getParameter("txtPrecio"));
+            int stock = Integer.parseInt(request.getParameter("txtStock"));
+            String categoria = request.getParameter("txtCategoria");
+            
+            //Mete los nuevos valores al molde usando el constructor completo
+            Refaccion refModificada = new Refaccion(id, codigo, nombre, descripcion, precio, stock, categoria);
+            
+            //El dao ejecuta el update en postgresql
+            rDao.actualizar(refModificada);
+            
+            //Redirige al listado limpio para ver los cambios en la tabla
+            response.sendRedirect("RefaccionController?accion=listar");
+        }
     }
     
     @Override
